4b212fa5b885e2fd580389e00c146afb5fd6ffa4
Eishika Mostafiz <eishika.hoque@ibm.com>
2025-01-29 18:56:50 -0500

    added article components

---
+++ blocks/custom/article-section/article-section.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+  const div = [block.children];
+  div.classList.add('article-section-quote');
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('p');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+  block.append(div);
+}
