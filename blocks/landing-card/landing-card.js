b19e4720c462b0fb4e9a77b21a4ca76a581bafed
Nissharfathima <38177009+Nissharfathima@users.noreply.github.com>
2025-02-05 02:02:55 +0530

    Fixed lint errors

--- blocks/landing-card/landing-card.js
+++ blocks/landing-card/landing-card.js
@@ -1,39 +1,37 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    
     row.classList.add('card');
-
-    let cardBackgroundImage = row.querySelectorAll('div')[0];
+    const cardBackgroundImage = row.querySelectorAll('div')[0];
     cardBackgroundImage.classList.add('card-background-image');
 
-    let cardHeader = row.querySelectorAll('div')[1];
+    const cardHeader = row.querySelectorAll('div')[1];
     cardHeader.classList.add('card-header');
 
-    let cardLinkIcon = row.querySelectorAll('div')[2];
-    cardLinkIcon.classList.add('card-link-icon')
-    
-    let iconLinkDiv = row.querySelectorAll('div')[row.querySelectorAll('div').length - 1]    
-    let iconLink = iconLinkDiv.querySelector('a');
-    if(iconLink) {
-      let href = iconLink.getAttribute("href"); // fetching href from anchor tag
-      let image = cardLinkIcon.querySelector('img')
-      let link = document.createElement('a');
+    const cardLinkIcon = row.querySelectorAll('div')[2];
+    cardLinkIcon.classList.add('card-link-icon');
+
+    const iconLinkDiv = row.querySelectorAll('div')[row.querySelectorAll('div').length - 1];
+    const iconLink = iconLinkDiv.querySelector('a');
+    if (iconLink) {
+      const href = iconLink.getAttribute('href'); // fetching href from anchor tag
+      const image = cardLinkIcon.querySelector('img');
+      const link = document.createElement('a');
       link.href = href;
-      link.target = "_blank";
+      link.target = '_blank';
       image.parentNode.insertBefore(link, image);
       link.appendChild(image);
       iconLink.style.display = 'none'; // removing the anchor tag from DOM
     }
 
-    let backgroundDiv = row.querySelector('.card-background-image');
-    let backgroundImage = backgroundDiv?.querySelector('img');
+    const backgroundDiv = row.querySelector('.card-background-image');
+    const backgroundImage = backgroundDiv?.querySelector('img');
     if (backgroundImage) {
-      let imageUrl = backgroundImage.getAttribute('src'); 
+      const imageUrl = backgroundImage.getAttribute('src');
       row.style.backgroundImage = `url('${imageUrl}')`;
-      row.style.backgroundSize = 'cover'; 
+      row.style.backgroundSize = 'cover';
       row.style.backgroundPosition = 'center';
       row.style.backgroundRepeat = 'no-repeat';
       backgroundDiv.remove();
     }
-  }
-)}
+  });
+}
