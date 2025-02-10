eb0cae3c423322eb5b93a9a3ea4c615672dfa8a8
rama-armoush <rama.ar@ibm.com>
2025-02-10 18:07:28 -0500

    Review comments applied

--- blocks/faq-section/faq-section.js
+++ blocks/faq-section/faq-section.js
@@ -1,24 +1,12 @@
 export default function decorate(block) {
-  // Get the parent element
   const sectionParent = block.parentNode.parentElement;
-
-  // Create a new wrapper div
   const newWrapper = document.createElement('div');
-  newWrapper.classList.add('faq-content-wrapper'); // Add class to the wrapper
-
-  // Get all child elements of the parent
+  newWrapper.classList.add('faq-content-wrapper');
   const children = Array.from(sectionParent.children);
-
-  // Check if there are at least three children
   if (children.length >= 2) {
-    // Append the first and second child divs to the new wrapper
-    newWrapper.appendChild(children[0]); // faq-section-wrapper
-    newWrapper.appendChild(children[1]); // accordion-wrapper
-
-    // Insert the new wrapper before the third child if it exists, or at the end
+    newWrapper.appendChild(children[0]);
+    newWrapper.appendChild(children[1]);
     sectionParent.insertBefore(newWrapper, children[2] || null);
   }
-
-  // Add a class to the parent block for additional styling
   block.classList.add('faq-container');
 }
