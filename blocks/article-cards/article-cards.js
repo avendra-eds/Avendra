9b9a763cbb86420d3e195ae273a101a3ed0c818c
Eishika Mostafiz <eishika.hoque@ibm.com>
2025-02-13 18:54:10 -0500

    fix comment changes

--- blocks/article-cards/article-cards.js
+++ blocks/article-cards/article-cards.js
@@ -1,3 +1,38 @@
+function decorateCardBody(articleCardBody) {
+  const tagContainer = document.createElement('div');
+  tagContainer.classList.add('article-card-tags');
+  articleCardBody.appendChild(tagContainer);
+
+  const pTags = articleCardBody.querySelectorAll('.button-container');
+  if (pTags) {
+    pTags.forEach((pTag) => {
+      if (pTag.firstElementChild.nodeName === 'EM') {
+        tagContainer.appendChild(pTag);
+      } else {
+        pTag.classList.remove('button-container');
+        pTag.firstElementChild.classList.remove('button');
+        pTag.classList.add('article-card-title');
+      }
+    });
+  }
+
+  const articleDescription = articleCardBody.querySelector('p:not([class])');
+  if (articleDescription) {
+    articleDescription.classList.add('article-card-description');
+  }
+
+  const articleDate = articleCardBody.querySelector('p > em');
+  if (articleDate) {
+    articleDate.classList.add('article-card-date');
+  }
+}
+
+function decorateCardImage(imageContainer) {
+  const image = imageContainer.querySelector('picture');
+  const imageLink = imageContainer.querySelector('a');
+  imageLink.appendChild(image);
+}
+
 export default function decorate(block) {
   [...block.children].forEach((card) => {
     card.classList.add('article-card');
@@ -10,41 +45,10 @@ export default function decorate(block) {
   });
 
   const articleCardsBody = block.querySelectorAll('.article-card-body');
-  articleCardsBody.forEach((articleCardBody) => {
-    const tagContainer = document.createElement('div');
-    tagContainer.classList.add('article-card-tags');
-    articleCardBody.appendChild(tagContainer);
-
-    const pTags = articleCardBody.querySelectorAll('.button-container');
-    if (pTags) {
-      pTags.forEach((pTag) => {
-        if (pTag.firstElementChild.nodeName === 'EM') {
-          tagContainer.appendChild(pTag);
-        } else {
-          pTag.classList.remove('button-container');
-          pTag.firstElementChild.classList.remove('button');
-          pTag.classList.add('article-card-title');
-        }
-      });
-    }
-
-    const articleDescription = articleCardBody.querySelector('p:not([class])');
-    if (articleDescription) {
-      articleDescription.classList.add('article-card-description');
-    }
-
-    const articleDate = articleCardBody.querySelector('p > em');
-    if (articleDate) {
-      articleDate.classList.add('article-card-date');
-    }
-  });
+  articleCardsBody.forEach(decorateCardBody);
 
   const articleCardImage = block.querySelectorAll('.article-card-image');
   if (articleCardImage) {
-    articleCardImage.forEach((imageContainer) => {
-      const image = imageContainer.querySelector('picture');
-      const imageLink = imageContainer.querySelector('a');
-      imageLink.appendChild(image);
-    });
+    articleCardImage.forEach(decorateCardImage);
   }
 }
