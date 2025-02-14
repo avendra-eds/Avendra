f2cdff9218acc136478c33107943936c74762cc8
Eishika Mostafiz <eishika.hoque@ibm.com>
2025-02-13 19:13:42 -0500

    merged resources

--- blocks/template-article/template-article.js
+++ blocks/template-article/template-article.js
@@ -1,6 +1,5 @@
-export default function decorate(section) {
-  const resourceSection = document.createElement('div');
-  const resources = section.querySelector('.article-cards-wrapper');
-  resourceSection.classList.add('resource-section');
-  resourceSection.append(resources);
-}
+const doc = document.querySelector('main');
+const resourceSection = document.createElement('div');
+const resources = doc.querySelector('.article-cards-wrapper');
+resourceSection.classList.add('resource-section');
+resourceSection.appendChild(resources);
